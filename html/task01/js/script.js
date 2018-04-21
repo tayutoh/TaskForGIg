17e58bfcbda6753b552729b47c8c0ae102bc3d74
Blueray777 <ray.00777@gmail.com>
2018-04-22 00:10:33 +0900

    [Task For GIG]Make Javascript

--- html/task01/js/script.js
+++ html/task01/js/script.js
@@ -1,7 +1,15 @@
 $(function(){
-	var $addList = $("button"),
-		$inputList = $("input[type=text]"),
-		$listArea = $("ul.main-block_listBox"),
-		$list = $("li.main-block_listBoxList");
+	var $addList = $('button'),
+		$inputList = $('input[type=text]'),
+		$listArea = $('ul.main-block_listBox');
+		
+	//ToDoを追加する	
+	function makeList(text){
+		//liを作成
+		var $li = $('<li class="main-block_listBoxList">');
+		var $text = $('<span class="main-block_listBoxListText">').text(text);
+		//liを追加
+		$li.append($text);	
+	}
 
 });
