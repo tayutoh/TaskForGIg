d2aba1fb482534bfb335d6b8887892728166a842
Blueray777 <ray.00777@gmail.com>
2018-04-22 15:58:23 +0900

    [Task For GIG] progress

--- html/task01/js/script.js
+++ html/task01/js/script.js
@@ -1,15 +1,63 @@
-$(function(){
-	var $addList = $('button'),
+$(function () {
+	var $form = $('#form-block'),
+		$addList = $('button'),
 		$inputList = $('input[type=text]'),
-		$listArea = $('ul.main-block_listBox');
+		$listArea = $('<ul class="main-block_listBox">');
 		
-	//ToDoを追加する	
+	//ToDoをul.listAreaに追加する	
 	function makeList(text){
 		//liを作成
-		var $li = $('<li class="main-block_listBoxList">');
-		var $text = $('<span class="main-block_listBoxListText">').text(text);
-		//liを追加
-		$li.append($text);	
+		var $li = $('<li class="main-block_listBoxList">'),
+			$text = $('<label class="main-block_listBoxListTxt">').text(text),
+			$checkBox = $('<input type="checkbox">'),
+			$deleteBtn = $('<span class="main-block_listBoxListBtn">Delete</span>'),
+			$removeMessege = $('.box-removeMess');
+		
+		//liに追加
+		$li.append($checkBox).append($text).append($deleteBtn);
+		//liを$listAreaに追加
+		$listArea.append($li);
+		
+		// checkBoxにチェックを入れたときの処理
+		$checkBox.on('click', function(){
+			if($(this).is(':checked')){
+				$li.addClass('complete');
+				$checkBox.prop('checked', true).prop('disabled', true);
+			} else {
+				$li.removeClass('complete');
+			}
+			
+		});
+		
+		//削除ボタンの処理
+		$deleteBtn.on('click', function(){
+			$removeMessege.addClass('box-removeMessVisible');
+			var $removeBtn = $('.box-removeMessBtn');
+			$removeBtn.click(function(){
+				$li.remove();
+				$removeMessege.removeClass('box-removeMessVisible');
+			});
+			
+		});
 	}
-
+	
+	//ボタンを送信する時のFormの処理
+	$($form).bind('submit', function(event){
+		
+		event.preventDefault();
+		
+		//$('.count_txt').text(count_num); 
+		//input[type="text"]の中に入っている文言を取得する
+		var text = $inputList.val();
+		
+		//ToDo list を追加
+		makeList(text);
+		
+		//input[type="text"]の中を空にする
+		$inputList.val('');
+	}).one('submit',function(){
+		//liを追加するulを最初のリスト追加時のみ追加する
+		$($form).after($listArea);
+		return false;
+	});
 });
