2aaf6ca095c68241dbf9dff6f94b450951948b9f
Blueray777 <ray.00777@gmail.com>
2018-05-06 15:26:28 +0900

    [TaskForGIg]
    [task02]script.js, global.css, index.htmlの追加と不用なデータの削除

--- html/task02/js/script.js
+++ html/task02/js/script.js
@@ -1,7 +1,35 @@
-$(function(){
-	var $addList = $("button"),
-		$inputList = $("input[type=text]"),
-		$listArea = $("ul.main-block_listBox"),
-		$list = $("li.main-block_listBoxList");
+$(document).ready(function(){
+	var $form = $('#form-block'),
+		$input = $('#form-block input[type="text"]'),
+		$main = $('<dl class="main"></dl>'),
+		$mainTerm = '<dt class="main-term">住所検索結果</dt>';
+	
+	//ボタンを送信する時のFormの処理
+	$($form).bind('submit', function (event) {
+		event.preventDefault();
+		///住所表示エリアを追加
+		$($form).after($main);
+		//zipcloudのAPIのURL
+		var sendURL = "http://zipcloud.ibsnet.co.jp/api/search?callback=?";
+		$.getJSON(sendURL,
+			{
+				zipcode: $input.val()
+			}
+		)
+		//結果を取得したら
+		.done(function(res){
+			// 中身が空でなければ、その値を［住所］欄に反映
+			if(res.results){
+				var results = res.results[0];
+				$($main).html($mainTerm + '<dd class="main-description">' + results.address1 + results.address2 + results.address3 + '</dd>');
+			} else {
+				//エラーの場合
+				$($main).html($mainTerm + '<dd class="main-description main-error">該当する住所が存在しません。</dd>');	
+				}
+		});
+
+		//input[type="text"]の中を空にする
+		$input.val('');
+	});
 
 });
